5856b31c92b51e1e6648b46f27b927591b609788
Lastman0413 <sehyok01@naver.com>
2025-08-20 09:20:08 +0900

    first commit

---
+++ a002.py
@@ -0,0 +1,17 @@
+# Git 과 Github
+
+"""
+
++git 설치 할 것 
+
+https://github.com/new
+
+깃허브 -> 원격 저장소
+
+로컬에서 한 작업을 git에 올려야 연동, 협업해서 작업 가능함
+
+git remote add origin https://github.com/OH-SE-HYEOK/GPT_Scraping.git
+
+
+
+"""
---
+++ a004.py
@@ -0,0 +1,17 @@
+"""
+깃 허브에 올리기
+
+* 그전에 gitignore란 : 어떤것들을 깃허브에 올릴때 보안과 관련된 것들은 깃허브에 올리지 않는다.
+예를 들어 이메일 키라든지 비밀번호 암호화 관련키들을 깃허브에 올라가면 깃허브는 공유공간이기에 
+헤커들이 내 자료에 들어와서 민감자료들을 빼갈수 있음. 그러므로 gitignore로 깃허브에 올리지 
+않을것들을 제외를 시킬수 있다. 그런데 구글에서 gitignore라는 사이트가 있는데 
+https://www.toptal.com/developers/gitignore
+여기서 python 치면 나오는 파일 복사해서 vsc의 gitignore에 붙여넣기 하면 된다.
+
+* git bash 에서 명령어 
+  git init : git init은 새로운 git 저장소를 생성하거나 기존 저장소를 초기화한다.
+  git remote add origin url : 원격 저장소 연결
+  원격과 로컬이 연결 되면 내 깃허브 주소를 알려주어야함
+
+
+"""
